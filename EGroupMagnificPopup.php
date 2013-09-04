2ded57eaebf4d4dbb62636b15fd94b0212541ca6
Edgard Lorraine Messias <edgardmessias@gmail.com>
2013-09-04 11:43:58 -0300

    Made widget from a group of elements with one parent;

---
+++ EGroupMagnificPopup.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * ## EGroupMagnificPopup class file.
+ *
+ * @author Edgard Lorraine Messias <edgardmessias@gmail.com>
+ * @copyright Copyright &copy; Edgard Lorraine Messias 2013-
+ */
+$dir = dirname(__FILE__);
+$alias = md5($dir);
+Yii::setPathOfAlias($alias, $dir);
+Yii::import($alias . '.EBaseMagnificPopup');
+
+/**
+ * Description of EGroupMagnificPopup
+ *
+ * @see http://dimsemenov.com/plugins/magnific-popup/
+ * @author Edgard Lorraine Messias <edgardmessias@gmail.com>
+ */
+class EGroupMagnificPopup extends EBaseMagnificPopup {
+
+    public $tagName = 'div';
+    public $htmlOptions = array();
+
+    public function init() {
+        $this->defaultOptions = CMap::mergeArray($this->defaultOptions, array(
+                    'delegate' => 'a',
+        ));
+        $this->target = '#' . $this->id;
+        echo CHtml::openTag($this->tagName, array('id' => $this->id), $this->htmlOptions);
+        parent::init();
+    }
+
+    /**
+     * Run this widget.
+     * This method registers necessary javascript.
+     */
+    public function run() {
+        echo CHtml::closeTag($this->tagName);
+        parent::run();
+    }
+
+}
+
+?>
